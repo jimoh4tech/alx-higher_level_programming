3588e04bae193f9e4b2d6515958cc6b11f9ddc6c
root <root@ABU-ABDILLAH.localdomain>
2022-12-04 21:26:56 +0100

    Fixed highlighted errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     """Print list of integers in revers"""
     for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+        print("{}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for mat in matrix:
-        l = len(mat)
-        for i in range(l):
-            print("{:d}".format(mat[i]), end=' ' if i != (l - 1) else '')
+        ln = len(mat)
+        for i in range(ln):
+            print("{:d}".format(mat[i]), end=' ' if i != (ln - 1) else '')
         print()
