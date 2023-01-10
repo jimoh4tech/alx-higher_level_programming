5d498ca4b2bab95b8875827dc6ce43d09ab6cf2c
root <root@ABU-ABDILLAH.localdomain>
2023-01-10 22:45:00 +0100

    Attempt to Python Input and output tasks

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A text file-reading function """
+
+
+def read_file(filename=""):
+    """Print the contents of a UTF8 text file to stdout """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
