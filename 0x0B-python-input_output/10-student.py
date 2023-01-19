1f3f696c8c29871161208c7b5045ac0b55a8750a
root <root@ABU-ABDILLAH.localdomain>
2023-01-19 13:43:28 +0100

    Minor fixes to some tasks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,16 +17,16 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs=None):
-            """Get a dictionary representation of the Student.
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
 
-            If attrs is a list of strings, represents only those attributes
-            included in the list.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
 
-            Args:
-            attrs (list): (Optional) The attributes to represent.
-            """
-            if (type(attrs) == list and
-                    all(type(ele) == str for ele in attrs)):
-                return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-            return self.__dict__
+        Args:
+         cccattrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+            all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
